26fc1da5747ef9640636c25983f7506b631c5082
a.pride <a.pride.msk@gmail.com>
2020-10-02 12:58:04 +0300

    , add constranits, SignUp Screen

--- iChat/SignUpViewController.swift
+++ iChat/SignUpViewController.swift
@@ -17,6 +17,10 @@ class SignUpViewController: UIViewController {
     let confirmPasswordLabel = UILabel(text: "Confirm Password")
     let alreadyOnboardLabel = UILabel(text: "Already onboard?")
     
+    let emailTextField = OneLineTextField(font: .avenir20())
+    let passwordTextField = OneLineTextField(font: .avenir20())
+    let confirmPasswordTextField = OneLineTextField(font: .avenir20())
+    
     let signUpButton = UIButton(title: "Sign Up", titleColor: .white, backgroundColor: .buttonDark(), cornerRadius: 4)
     let loginButton = UIButton()
     
@@ -25,7 +29,58 @@ class SignUpViewController: UIViewController {
         
         loginButton.setTitle("Login", for: .normal)
         loginButton.setTitleColor(.buttonRed(), for: .normal)
-        view.backgroundColor = .systemRed
+        loginButton.titleLabel?.font = .avenir20()
+        
+        setupConstraints()
+    }
+    
+}
+
+// MARK: Setup Constraints
+extension SignUpViewController {
+    private func setupConstraints() {
+        
+        let emailStackView = UIStackView(arrangedSubviews: [emailLabel,emailTextField ], axis: .vertical, spacing: 0)
+        let passwordStackView = UIStackView(arrangedSubviews: [passwordLabel, passwordTextField], axis: .vertical, spacing: 0)
+        let confirmPassordStackView = UIStackView(arrangedSubviews: [confirmPasswordLabel, confirmPasswordTextField], axis: .vertical, spacing: 0)
+        
+         signUpButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [emailStackView, passwordStackView, confirmPassordStackView, signUpButton],
+                                    axis: .vertical,
+                                    spacing: 40)
+        
+        let bottomStackView = UIStackView(arrangedSubviews: [alreadyOnboardLabel, loginButton],
+                                          axis: .horizontal,
+                                          spacing: -50)
+        
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(welcomeLabel)
+        view.addSubview(stackView)
+        view.addSubview(bottomStackView)
+        
+        NSLayoutConstraint.activate([
+            welcomeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 160),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        
+        ])
+        
+        NSLayoutConstraint.activate([
+            bottomStackView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 60),
+            bottomStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            bottomStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        
     }
 }
 
