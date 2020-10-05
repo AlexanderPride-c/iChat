a3aca7ede94e4d9eb554984e274c21ab530ca762
a.pride <a.pride.msk@gmail.com>
2020-10-05 12:34:33 +0300

    AddPhotoView

---
+++ iChat/Extension/AddPhotoView.swift
@@ -0,0 +1,32 @@
+//
+//  AddPhotoView.swift
+//  iChat
+//
+//  Created by Александр Прайд on 05.10.2020.
+//  Copyright © 2020 Alexander Pride. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoView: UIView {
+    
+    var circleImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = #imageLiteral(resourceName: "avatar")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.borderColor = UIColor.black.cgColor
+        imageView.layer.borderWidth = 1
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
