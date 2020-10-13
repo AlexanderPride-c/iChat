86f70cebbebb58bfc063f118c42f13bbd7688479
a.pride <a.pride.msk@gmail.com>
2020-10-13 16:54:29 +0300

    MainTabBarViewController created

---
+++ iChat/Controllers/MainTabBarViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MainTabBarViewController.swift
+//  iChat
+//
+//  Created by Александр Прайд on 13.10.2020.
+//  Copyright © 2020 Alexander Pride. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listVC = ListViewController()
+        let peopleVC = PeopleViewController()
+        
+        tabBar.tintColor = #colorLiteral(red: 0.5568627451, green: 0.3529411765, blue: 0.968627451, alpha: 1)
+        let boldConfig = UIImage.SymbolConfiguration(weight: .medium)
+        let convImage = UIImage(systemName: "bubble.left.and.bubble.right", withConfiguration: boldConfig)!
+        let peopleImage = UIImage(systemName: "person.2", withConfiguration: boldConfig)!
+        
+        viewControllers = [
+            generateNavigationController(rootViewController: listVC, title: "Conversations", image: convImage),
+            generateNavigationController(rootViewController: peopleVC, title: "People", image: peopleImage)
+        
+        ]
+    }
+    
+    private func generateNavigationController(rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = image
+        return navigationVC
+    }
+}
---
+++ iChat/Controllers/PeopleViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PeopleViewController.swift
+//  iChat
+//
+//  Created by Александр Прайд on 13.10.2020.
+//  Copyright © 2020 Alexander Pride. All rights reserved.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
